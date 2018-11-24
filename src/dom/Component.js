1bc2669e6d232a537c7b53f363f70fd6fcc75cd2
DimaDK <DimaKrutDK@ya.ru>
2018-11-24 21:26:32 +0300

    chore: small class to easier control of components

---
+++ src/dom/Component.js
@@ -0,0 +1,13 @@
+import { createElementWithClasses } from './helpers';
+
+class Component {
+  constructor(elementName, ...classes) {
+    this.element = createElementWithClasses(elementName, ...classes);
+  }
+
+  static getElements(components) {
+    return components.map(component => component.element);
+  }
+}
+
+export default Component;
