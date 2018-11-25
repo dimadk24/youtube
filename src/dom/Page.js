ebec47e385a944afb92e2d844fd18609b5a55026
DimaDK <DimaKrutDK@ya.ru>
2018-11-25 22:05:13 +0300

    feat: basic functionality (only first 15 videos)

--- src/dom/Page.js
+++ src/dom/Page.js
@@ -1,16 +1,76 @@
-import { appendChildren, createDivWithClasses } from './helpers';
+import { createDivWithClasses } from './helpers';
 import SearchBar from './SearchBar';
 import Slider from './Slider';
 
+const YOUTUBE_KEY = 'AIzaSyAoxmNlzlKwuTNRMvWITXvtFpc-7vqfcr8';
+const API_URL = 'https://www.googleapis.com/youtube/v3';
+
+function getVideoIds(videos) {
+  const stringIds = videos.reduce((accumulator, video) => `${accumulator},${String(video.id)}`, '');
+  return stringIds.slice(1);
+}
+
+async function loadViews(ids) {
+  const response = await fetch(`${API_URL}/videos?key=${YOUTUBE_KEY}&type=video&part=statistics&maxResults=15&id=${ids}`,
+    { method: 'GET' });
+  const message = await response.json();
+  return message;
+}
+
+async function loadVideos(query) {
+  const response = await fetch(`${API_URL}/search?key=${YOUTUBE_KEY}&type=video&part=snippet&maxResults=15&q=${query}`,
+    { method: 'GET' });
+  const message = await response.json();
+  return message;
+}
+
+function convertVideos(youtubeVideos) {
+  return youtubeVideos.map((video) => {
+    const { snippet } = video;
+    return {
+      id: video.id.videoId,
+      title: snippet.title,
+      preview: snippet.thumbnails.medium.url,
+      author: snippet.channelTitle,
+      date: snippet.publishedAt,
+      views: 0,
+      description: snippet.description,
+    };
+  });
+}
+
+function addViewsToVideos(videos, views) {
+  return videos.map((video, index) => {
+    const newVideo = video;
+    newVideo.views = views[index];
+    return video;
+  });
+}
+
+function convertViews(youtubeViews) {
+  return youtubeViews.map(view => view.statistics.viewCount);
+}
 
 class Page {
-  constructor(videos) {
-    const wrapper = createDivWithClasses('container');
-    const searchBar = new SearchBar();
+  constructor() {
+    this.wrapper = createDivWithClasses('container');
+    const searchBar = new SearchBar(this.onStartSearch.bind(this));
+    this.wrapper.appendChild(searchBar.element);
+    document.body.appendChild(this.wrapper);
+  }
+
+  async onStartSearch(query) {
+    const youtubeResponse = await loadVideos(query);
+    let videos = convertVideos(youtubeResponse.items);
+    const ids = getVideoIds(videos);
+    const youtubeViews = await loadViews(ids);
+    const views = convertViews(youtubeViews.items);
+    videos = addViewsToVideos(videos, views);
     const slider = new Slider(videos);
-    appendChildren(wrapper, [searchBar.element, slider.element]);
-    document.body.appendChild(wrapper);
+    this.wrapper.appendChild(slider.element);
   }
 }
 
-export default Page;
+export default function page() {
+  return new Page();
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import './utils.css';
 import './index.css';
 import './fontello/css/fontello.css';
-import Page from './dom/Page';
+import page from './dom/Page';
 
-const video = {
-  title: 'lorem ipsum',
-  description: 'description',
-  preview: 'https://wallpapersgood.com/wallpapers/preview/201402/28a57d5f9571301.jpg',
-  author: 'DimaDK',
-  date: 'today',
-  views: 123,
-};
-const videos = Array(8);
-videos.fill(video);
-// eslint-disable-next-line no-new
-new Page(videos);
+page();
