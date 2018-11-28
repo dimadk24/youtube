0a93a85bf10f831386a5b302a76df54ab6ad36fa
DimaDK <DimaKrutDK@ya.ru>
2018-11-28 21:20:26 +0300

    refactor: move loaders to separate file

--- src/logic/Page.js
+++ src/logic/Page.js
@@ -1,35 +1,7 @@
-import { createDivWithClasses } from '../dom/helpers/dom-helpers';
 import SearchBar from '../dom/Components/SearchBar/SearchBar';
 import Slider from '../dom/Slider';
-import {
-  getVideoIds,
-  addViewsToVideos,
-  convertVideos,
-  convertViews,
-} from './logic-helpers';
-
-const YOUTUBE_KEY = 'AIzaSyAoxmNlzlKwuTNRMvWITXvtFpc-7vqfcr8';
-const API_URL = 'https://www.googleapis.com/youtube/v3';
-
-let nextPageToken = '';
-
-async function loadViews(ids) {
-  const response = await fetch(`${API_URL}/videos?key=${YOUTUBE_KEY}&type=video&part=statistics&maxResults=15&id=${ids}`,
-    { method: 'GET' });
-  const message = await response.json();
-  return message;
-}
-
-async function loadVideos(query) {
-  let url = `${API_URL}/search?key=${YOUTUBE_KEY}&type=video&part=snippet&maxResults=15&q=${query}`;
-  if (nextPageToken) url += `&pageToken=${nextPageToken}`;
-  const response = await fetch(url, { method: 'GET' });
-  const message = await response.json();
-  // have to suppress it, cause I can't destruct it here
-  // eslint-disable-next-line prefer-destructuring
-  nextPageToken = message.nextPageToken;
-  return message;
-}
+import loadVideos from './loaders';
+import { createDivWithClasses } from '../dom/helpers/dom-helpers';
 
 class Page {
   constructor() {
@@ -55,12 +27,7 @@ class Page {
 
   async loadVideosWithViews() {
     this.loading = true;
-    const youtubeResponse = await loadVideos(this.query);
-    let videos = convertVideos(youtubeResponse.items);
-    const ids = getVideoIds(videos);
-    const youtubeViews = await loadViews(ids);
-    const views = convertViews(youtubeViews.items);
-    videos = addViewsToVideos(videos, views);
+    const videos = await loadVideos(this.query);
     this.loading = false;
     return videos;
   }
